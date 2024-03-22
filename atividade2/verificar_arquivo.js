931ca8796cd1299c864444ee37cf8b6db66927bd
JORGE IGOR BARROS GOMES <jorge.gomes@a.ucb.br>
2024-03-22 10:14:25 -0300

    atv 1

---
+++ atividade2/verificar_arquivo.js
@@ -0,0 +1,13 @@
+let a = 8;
+let b = 6;
+let ope = a + b;
+console.log(`O resultado da soma de ${a} e ${b} é igual a ${ope} `);
+ope = a - b;
+console.log(`O resultado da subtração de ${a} e ${b} é igual a ${ope} `);
+ope = a * b;
+console.log(`O resultado da multiplicação de ${a} e ${b} é igual a ${ope} `);
+ope = a / b;
+console.log(`O resultado da divisao de ${a} e ${b} é igual a ${ope} `);
+
+
+
